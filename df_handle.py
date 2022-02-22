a76cbe4f34ba7bcb8a294b88278ec9b65b4e0dab
Felix Trinler <felix@MacBook-Pro-von-Felix.local>
2022-02-22 11:34:06 +0100

    Added doctrings to df_handle.py

--- df_handle.py
+++ df_handle.py
@@ -2,16 +2,25 @@ import pandas as pd
 
 class DataframeHandle(pd.DataFrame):
     """
-    
+    Creates the dataframe of a csv-file or creates a dataframe without the first column.
+
+    Attributes:
+        csv_file_location: the path to the csv-file
     """
     def __init__(self, csv_file_location):
+        """ 
+        Initializes the class.
+
+        Arguments:
+            csv_file_location: the path to the csv-file
+        """
         super().__init__()
         self.csv_file_location = csv_file_location
         self.create_df_csv()
 
     def create_df_csv(self):
         """
-        
+        Creates the dataframe of a csv-file.
         """
         try:
             new_df = pd.read_csv(self.csv_file_location)
@@ -24,7 +33,7 @@ class DataframeHandle(pd.DataFrame):
     
     def create_df_wo_x(self):
         """
-        
+        Creates a dataframe without the first column out of a already existing dataframe.
         """
         self.df_wo_x = self.iloc[:, 1:]
         return self.df_wo_x
